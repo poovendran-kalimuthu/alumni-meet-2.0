83a5b0a187e528426d5128b35a075ff0d84c6a1d
Poovendran-Hari <hustlesmarket@gmail.com>
2026-02-15 18:20:12 +0530

    maid

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import authRoutes from "./src/routes/auth.route.js";
+import dotenv from 'dotenv';
+import { connectDB } from './src/lib/db.js';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors({
+    
+    origin: true,   
+    credentials: true
+
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.listen(PORT, "0.0.0.0", async () => {
+    console.log("Server is connected to the PORT : " + PORT);
+    await connectDB();
+    console.log("Happy Hacking !!");
+});
+    
---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import { useAuthStore } from './store/useAuthStore'
+import { Toaster } from 'react-hot-toast'
+import { Loader } from 'lucide-react'
+import AdminLogin from './pages/AdminPage'
+import AdminDashboard from './pages/AdminDashboard'
+import { ToastContainer, toast } from 'react-toastify';
+
+const App = () => {
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({authUser});
+
+  if (isCheckingAuth)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element={authUser ? <HomePage/> : <Navigate to="/login" />} />
+          <Route path = "/signup" element={!authUser ? <SignupPage/> : <Navigate to="/" />} />
+          <Route path = "/login" element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
+          <Route path = "/portal/admin" element={<AdminLogin/>} />
+          <Route path = "/portal/admin/dashboard" element={<AdminDashboard/>} />
+        </Routes>
+        <ToastContainer/>
+      </BrowserRouter>
+      <Toaster />
+    </div>
+  )
+}
+
+export default App
